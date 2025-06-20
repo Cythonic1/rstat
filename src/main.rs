0eb7cb1955766d1c35391ec8afe3d60a9fe5a8d0
Cythonic1 <jfjbn80@gmail.com>
2025-06-21 03:42:43 +0800

    simple modification and updating

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
+use clap::Parser;
 
-use clap::{command, Parser};
-
-#[derive(Parser,Debug)]
-#[command(version, about, long_about = "Improve stat version")]
+/// Simple CLI to show file metadata
+#[derive(Parser, Debug)]
 struct Args {
-    #[arg(short, long, help = "file name")]
-    file_name: PathBuf
+    /// File name to inspect
+    file_name: PathBuf,
+}
+
+fn print_data(file_name: &str) {
+    match fs::metadata(file_name) {
+        Ok(metadata) => {
+            println!("File Metadata for '{}':", file_name);
+            println!("  - File size: {} bytes", metadata.len());
+            println!("  - Is directory: {}", metadata.is_dir());
+            println!("  - Is file: {}", metadata.is_file());
+            println!("  - Permissions: {:?}", metadata.permissions());
+            #[cfg(unix)]
+            {
+                use std::os::unix::fs::MetadataExt;
+                println!("  - Inode: {}", metadata.ino());
+                println!("  - Mode: {:o}", metadata.mode());
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to get metadata for '{}': {}", file_name, e);
+        }
+    }
 }
 
 fn main() {
     let cli = Args::parse();
 
     println!("Thank you for the file: {:?}", cli.file_name);
+    print_data(cli.file_name.to_str().unwrap());
 
-    match std::fs::metadata(cli.file_name){
-        Ok(data) =>  {
-            println!("{:?}", data)
-        },
-        Err(err) => {
-            eprintln!("Error {err}");
-        }
-
-    }
     println!("Hello, world!");
 }
