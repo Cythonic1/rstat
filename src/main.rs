698a52a4f6008f69db72eb3b0d707730454a281d
Cythonic1 <jfjbn80@gmail.com>
2025-07-05 21:36:32 +0800

    complete version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::{
     fs::{self, Metadata},
     os::unix::fs::MetadataExt,
-    path::PathBuf,
+    path::{Path, PathBuf},
     time::SystemTime,
 };
 use clap::Parser;
 use clap::Subcommand;
+use serde::{Deserialize, Serialize};
 use tabled::{settings::Style, Tabled};
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, TimeZone, Utc};
+use tree_magic_mini::{self, from_filepath};
+use users::{get_user_by_uid, get_group_by_gid};
 
 /// Simple CLI to show file metadata
 #[derive(Parser, Debug)]
@@ -28,7 +31,7 @@ enum PrintType {
     Normal,
 }
 
-#[derive(Debug, Tabled, Default)]
+#[derive(Debug, Tabled, Default, Serialize, Deserialize)]
 struct FileData {
     name: String,
     size: u64,
@@ -39,20 +42,52 @@ struct FileData {
     inod: u64,
     links: u64,
     access: String,
-    uid: u32,
-    gid: u32,
+    uid: String,
+    gid: String,
     sympolic: bool,
     access_date: String,
     modify_date: String,
     change_data: String,
     birth: String,
+    file_type: &'static str,
 }
 
-#[derive(Debug, strum::Display, Default)]
+impl std::fmt::Display for FileData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "File Name: {}", self.name)?;
+        writeln!(f, "Size: {} bytes", self.size)?;
+        writeln!(f, "Blocks: {}", self.blocks)?;
+        writeln!(f, "IO Block Size: {}", self.io_block)?;
+        writeln!(f, "Devices: {}", self.devices)?;
+        writeln!(f, "Entry Type: {:?}", self.entry_type)?;
+        writeln!(f, "Inode: {}", self.inod)?;
+        writeln!(f, "Links: {}", self.links)?;
+        writeln!(f, "Access Permissions: {}", self.access)?;
+        writeln!(f, "UID: {}", self.uid)?;
+        writeln!(f, "GID: {}", self.gid)?;
+        writeln!(f, "Symbolic Link: {}", self.sympolic)?;
+        writeln!(f, "Access Date: {}", self.access_date)?;
+        writeln!(f, "Modify Date: {}", self.modify_date)?;
+        writeln!(f, "Change Date: {}", self.change_data)?;
+        writeln!(f, "Birth Date: {}", self.birth)?;
+        writeln!(f, "Detected File Type: {}", self.file_type)
+    }
+}
+
+#[derive(Debug, strum::Display, Default, Serialize, Deserialize)]
 enum Type {
     Dir,
     #[default]
     File,
+}
+
+fn from_unix_to_normal_time(time: i64)  -> String{
+
+    // Convert to DateTime<Utc>
+    let dt: DateTime<Utc> = Utc.timestamp_opt(time, 0).unwrap();
+
+    // Format the DateTime<Utc> to a readable string
+    dt.format("%Y-%m-%d %H:%M:%S").to_string()
 }
 
 fn format_time(time: Result<SystemTime, std::io::Error>) -> String {
@@ -71,6 +106,7 @@ fn extract_data(metadata: Metadata, file_name: &str) -> FileData {
     } else {
         Type::File
     };
+    let file_path = Path::new(file_name);
 
     FileData {
         name: file_name.to_string(), 
@@ -82,17 +118,18 @@ fn extract_data(metadata: Metadata, file_name: &str) -> FileData {
         inod: metadata.ino(),
         links: metadata.nlink(),
         access: format!("{:?}", metadata.permissions()),
-        uid: metadata.uid(),
-        gid: metadata.gid(),
+        uid: format!("{:?}",get_user_by_uid(metadata.uid()).unwrap()),
+        gid: format!("{:?}", get_group_by_gid(metadata.gid()).unwrap()),
         sympolic: metadata.file_type().is_symlink(),
         access_date: format_time(metadata.accessed()),
         modify_date: format_time(metadata.modified()),
-        change_data: metadata.ctime().to_string(),
+        change_data: from_unix_to_normal_time(metadata.ctime()),
         birth: format_time(metadata.created()),
+        file_type: from_filepath(file_path).unwrap()
     }
 }
 
-fn print_data(file_name: &str) {
+fn print_data_table(file_name: &str) {
     match fs::metadata(file_name) {
         Ok(metadata) => {
             let data = extract_data(metadata, file_name);
@@ -106,17 +143,45 @@ fn print_data(file_name: &str) {
     }
 }
 
+fn print_data_normal(file_name: &str) {
+    match fs::metadata(file_name) {
+        Ok(metadata) => {
+            let data = extract_data(metadata, file_name);
+            println!("{}",data);
+        }
+        Err(e) => {
+            eprintln!("Failed to get metadata for '{}': {}", file_name, e);
+        }
+    }
+}
+
+fn print_data_json(file_name: &str)  {
+    match fs::metadata(file_name) {
+        Ok(metadata) => {
+            let data = extract_data(metadata, file_name);
+            let json = serde_json::to_string_pretty(&data).unwrap();
+            println!("{:#}",json);
+        }
+        Err(e) => {
+            eprintln!("Failed to get metadata for '{}': {}", file_name, e);
+        }
+    }
+
+}
 fn main() {
     let cli = Args::parse();
     let style = cli.style.unwrap_or(PrintType::Normal);
 
 
     match style {
-        PrintType::Normal => {
-            print_data(cli.file_name.to_str().unwrap_or("."));
+        PrintType::Table => {
+            print_data_table(cli.file_name.to_str().unwrap_or("."));
         },
+        PrintType::Json => {
+            print_data_json(cli.file_name.to_str().unwrap_or("."));
+        }
         _ => {
-            todo!()
+            print_data_normal(cli.file_name.to_str().unwrap_or("."));
         }
     }
 }
